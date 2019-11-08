21cb24357d65213fc150ca14c2521fffeb90b676
cristiansep <cristiansepulvedamendez@gmail.com>
2019-11-07 23:24:51 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -4,3 +4,27 @@
 //           Puerto
 //=====================================
 process.env.PORT = process.env.PORT || 3000;
+
+
+//=====================================
+//           Entorno
+//=====================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//=====================================
+//           Entorno
+//=====================================
+
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev'){
+
+    urlDB ='mongodb://localhost:27017/cafe'
+}else{
+    urlDB = 'mongodb+srv://cristiansep:<OeukAiAhVQlSesvl>@cluster0-y4is6.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB;
+
+
